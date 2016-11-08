69d6ae13d8a900b1cde0eddd92246767e306cb56
James Marshall <jamesomarshall@gmail.com>
2016-11-08 18:05:11 -0500

    creating the basic documents for the project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//variable that controls the background color
+
+//a click box that controls the background color variable
+
+	//on click a static variable will be injected into the background variable
+	//by pressing enter a text value will be placed in the background variable
+	//by moving a slider a color vairable will be injected into the background variable
+
